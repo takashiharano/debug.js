161241574ef32d9029d7ef3364f40462d5385025
Takashi Harano <git@takashiharano.com>
2016-01-29 00:15:02 +0900

    Update: add clear button setting

--- debug.js
+++ debug.js
@@ -5,7 +5,7 @@
  * Released under the MIT license
  * https://github.com/takashiharano/debug.js
  *
- * Date: 2016-01-27T21:34+09:00
+ * Date: 2016-01-29T00:05+09:00
  */
 var Debug = null;
 
@@ -13,13 +13,15 @@ function DebugJS() {
 	this.DEFAULT_DEBUG_ARIA_ID = 'debug';
 	this.DEFAULT_BUFFER_SIZE = 10;
 	this.DEFAULT_SHOW_LINE_NUMS = true;
+	this.DEFAULT_SHOW_CLEAR_BUTTON = true;
 }
 
 DebugJS.prototype = {
-	init:  function(messageAreaId, messageBufferSize, showLineNums) {
+	init:  function(messageAreaId, messageBufferSize, showLineNums, showClearButton) {
 		this.DebugMessageBuffer = new RingBuffer(messageBufferSize);
 		this.DebugMessageArea = document.getElementById(messageAreaId);
 		this.DebugIsShowLineNums = showLineNums;
+		this.DebugShowClearButton = showClearButton;
 
 		var selector = '#' + messageAreaId;
 		var styles = {};
@@ -31,21 +33,24 @@ DebugJS.prototype = {
 			'font-family': 'Consolas',
 			'font-size': '9pt',
 			'color': '#fff',
-			'background': '#000'
+			'background': '#111'
 		};
 
 		selector = '#' + messageAreaId + ' a';
 		styles[selector] = {'color': '#00bfff'};
-		applyStyles(styles);
+		this.applyStyles(styles);
 
 		this.clearMessage();
 	},
 
 	printMessage: function() {
 		var buf = this.DebugMessageBuffer.getAll();
-		var msg = '<a href="#" onclick="Debug.clearMessage();">[clear]</a><br/>';
+		var msg = '';
+		if (this.DebugShowClearButton) {
+			msg += '<a href="#" onclick="Debug.clearMessage();">[clear]</a><br/>';
+		}
 		for (var i = 0; i < buf.length; i++) {
-			msg = msg + buf[i] + '<br/>';
+			msg += buf[i] + '<br/>';
 		}
 		this.DebugMessageArea.innerHTML = msg;
 	},
@@ -53,6 +58,27 @@ DebugJS.prototype = {
 	clearMessage: function() {
 		this.DebugMessageBuffer.clear();
 		this.printMessage();
+	},
+
+	applyStyles: function(styles) {
+		var styleEl = document.createElement('style');
+		document.head.appendChild(styleEl);
+		styleEl.appendChild(document.createTextNode(''));
+		var s = styleEl.sheet;
+		for (var selector in styles) {
+			var props = styles[selector];
+			var propStr = '';
+			for (var propName in props) {
+				var propVal = props[propName];
+				var propImportant = '';
+				if (propVal[1] === true) {
+					propVal = propVal[0];
+					propImportant = ' !important'
+				}
+				propStr += propName + ':' + propVal + propImportant + ';\n';
+			}
+			s.insertRule(selector + '{' + propStr + '}', s.cssRules.length);
+		}
 	}
 };
 
@@ -109,7 +135,7 @@ RingBuffer.prototype = {
 			cnt++; //start at 1
 
 			if (Debug.DebugIsShowLineNums) {
-				var diffDigits = digits(maxCnt) - digits(cnt);
+				var diffDigits = this.digits(maxCnt) - this.digits(cnt);
 				padding = '';
 				for (var j = 0; j < diffDigits; j++) {
 					padding = padding + '0';
@@ -134,60 +160,39 @@ RingBuffer.prototype = {
 		this.buffer = new Array(this.buffer.length);
 		this.count = 0;
 		return;
+	},
+
+	digits: function(x) {
+		var digit = 0;
+		while (x != 0) {
+			x = (x / 10) << 0;
+			digit++;
+		}
+		return digit;
 	}
 };
-
-function digits(x) {
-	var digit = 0;
-	while (x != 0) {
-		x = (x / 10) << 0;
-		digit++;
-	}
-	return digit;
-};
-
-function debug(msg) {
-	if (Debug) {
-		Debug.DebugMessageBuffer.add(msg);
-		Debug.printMessage();
-	} else {
-		Debug = new DebugJS();
-		Debug.init(Debug.DEFAULT_DEBUG_ARIA_ID, Debug.DEFAULT_BUFFER_SIZE, Debug.DEFAULT_SHOW_LINE_NUMS);
-	}
-}
 
 function DebugAddEventListener(func) { 
 	try { 
 		window.addEventListener('load', func, false);
 	} catch (e) { 
-		window.attachEvent('onload', func);  // for IE
+		window.attachEvent('onload', func);  //for IE
 	}
 }
 
 function DebugInit() {
 	Debug = new DebugJS();
-	Debug.init(Debug.DEFAULT_DEBUG_ARIA_ID, Debug.DEFAULT_BUFFER_SIZE, Debug.DEFAULT_SHOW_LINE_NUMS);
+	Debug.init(Debug.DEFAULT_DEBUG_ARIA_ID, Debug.DEFAULT_BUFFER_SIZE, Debug.DEFAULT_SHOW_LINE_NUMS, Debug.DEFAULT_SHOW_CLEAR_BUTTON);
 }
 
-function applyStyles(styles) {
-	var styleEl = document.createElement('style');
-	document.head.appendChild(styleEl);
-	styleEl.appendChild(document.createTextNode(''));
-	var s = styleEl.sheet;
-	for (var selector in styles) {
-		var props = styles[selector];
-		var propStr = '';
-		for (var propName in props) {
-			var propVal = props[propName];
-			var propImportant = '';
-			if (propVal[1] === true) {
-				propVal = propVal[0];
-				propImportant = ' !important'
-			}
-			propStr += propName + ':' + propVal + propImportant + ';\n';
-		}
-		s.insertRule(selector + '{' + propStr + '}', s.cssRules.length);
+function debug(msg) {
+	if (!Debug) {
+		Debug = new DebugJS();
+		Debug.init(Debug.DEFAULT_DEBUG_ARIA_ID, Debug.DEFAULT_BUFFER_SIZE, Debug.DEFAULT_SHOW_LINE_NUMS, Debug.DEFAULT_SHOW_CLEAR_BUTTON);
 	}
+	Debug.DebugMessageBuffer.add(msg);
+	Debug.printMessage();
+
 }
 
 DebugAddEventListener(DebugInit);
