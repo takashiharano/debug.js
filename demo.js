a0e2afe8a9eadfddc1cc7bf596b0d16e33305d02
Takashi Harano <git@takashiharano.com>
2016-05-14 00:17:59 +0900

    Update: demo

--- demo.js
+++ demo.js
@@ -29,20 +29,13 @@ window.onload = function() {
 }
 
 document.onkeydown = function(e) {
-//  Debug.setStyle("color", "#0f0");
   log('KeyDown: keyCode = ' + e.keyCode);
-  log.e('error log');
-  log.w('warn log');
-  log.i('info log');
-  log.d('debug log');
-  log.v('verbose log');
-  log.s('special log');
 }
 
 document.onmousemove = function(e) {
-  log('mouseMoved: x=' + e.clientX + ', y=' + e.clientY);
+  //log('mouseMoved: x=' + e.clientX + ', y=' + e.clientY);
 };
 
 document.onclick = function(e) {
-  log('click count: ' + event.detail);
+  //log('click count: ' + event.detail);
 }
