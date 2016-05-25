d91b6a62119b30a5027ebac5886a6ff3abb45867
Takashi Harano <git@takashiharano.com>
2016-05-26 01:00:29 +0900

    Update: demo & description

--- demo.js
+++ demo.js
@@ -50,29 +50,32 @@ var h = [
 
 window.onload = function() {
 //in the absence of this initialization, debug.js works in default configuration.
-//  var options = {
-//    'buffSize': 10,
-//    'width': 500,
-//    'errorColor': '#d44',
-//    'warnColor': '#ed0',
-//    'infoColor': '#fff',
-//    'debugColor': '#8cf',
-//    'verboseColor': '#ccc',
-//    'specialColor': '#fff',
-//    'timeColor': '#0f0',
-//    'systemInfoColor': '#ddd',
-//    'showLineNums': true,
-//    'showTimeStamp': false,
-//    'showClock': true,
-//    'showClearButton': true,
-//    'showCloseButton': false,
-//    'showWindowSize': false,
-//    'showMousePosition': false,
-//    'showKeyStatus': true,
-//    'enableStopWatch': false,
-//    'enableCommandLine': false
-//  };
-//  Debug.init(null, options);
+  var options = {
+    'buffSize': 18,
+    'width': 500,
+    'position': 'center', // left-top, left-bottom, center, right-top, right-bottom
+    'posAdjX': 0,
+    'posAdjY': 0,
+    'errorColor': '#d44',
+    'warnColor': '#ed0',
+    'infoColor': '#fff',
+    'debugColor': '#8cf',
+    'verboseColor': '#ccc',
+    'specialColor': '#fff',
+    'timeColor': '#0f0',
+    'systemInfoColor': '#ddd',
+    'showLineNums': true,
+    'showTimeStamp': true,
+    'showClock': true,
+    'showClearButton': true,
+    'showCloseButton': true,
+    'showWindowSize': true,
+    'showMouseStatus': true,
+    'showKeyStatus': true,
+    'enableStopWatch': true,
+    'enableCommandLine': true
+  };
+  Debug.init(null, options);
 
 // If you want to output the logs to a designated area, pass the element id to 1st argument.
 //  Debug.init('test1', options);
