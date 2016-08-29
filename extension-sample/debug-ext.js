cf47b172a93379ccac54dbe8be82fbc7f00dea05
Takashi Harano <git@takashiharano.com>
2016-08-30 07:34:41 +0900

    Fix: function table definition

--- extension-sample/debug-ext.js
+++ extension-sample/debug-ext.js
@@ -1,5 +1,5 @@
 DebugJS.ext = {};
-DebugJS.ext.v = '201608300130';
+DebugJS.ext.v = '201608300733';
 
 DebugJS.ext.cmdTest = function() {
   DebugJS.log('test');
@@ -10,6 +10,6 @@ DebugJS.ext.cmdV = function() {
 };
 
 DebugJS.ext.CMD_TBL = [
-  {'cmd': 'test', 'fnc': DebugJS.ext.cmdTest1, 'desc': 'extention\'s command example'},
+  {'cmd': 'test', 'fnc': DebugJS.ext.cmdTest, 'desc': 'extention\'s command example'},
   {'cmd': 'extv', 'fnc': DebugJS.ext.cmdV, 'desc': 'Displays extension version info'}
 ];
