a00c9b39105fba64c5ab438dfdc5a3d778ca076a
Takashi Harano <takashiharano@users.noreply.github.com>
2019-06-22 21:56:47 +0900

    mod: HTML Template

--- debug.js
+++ debug.js
@@ -5,7 +5,7 @@
  * https://debugjs.net/
  */
 var DebugJS = DebugJS || function() {
-  this.v = '201906221554';
+  this.v = '201906222136';
 
   this.DEFAULT_OPTIONS = {
     visible: false,
@@ -680,7 +680,7 @@ DebugJS.HTML_SNIPPET = [
 '<button onclick=""></button>',
 '<video src="" controls autoplay>',
 '',
-'<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta charset="utf-8">\n<meta name="robots" content="none">\n<meta name="referrer" content="no-referrer">\n<meta name="referrer" content="never">\n<title></title>\n<link rel="stylesheet" href="style.css" />\n<script src="script.js"></script>\n<style>\n</style>\n<script>\n</script>\n</head>\n<body>\n\n</body>\n</html>\n'
+'<!DOCTYPE html>\n<html>\n<head>\n<meta http-equiv="X-UA-Compatible" content="IE=edge">\n<meta charset="utf-8">\n<meta name="robots" content="none">\n<meta name="referrer" content="no-referrer">\n<meta name="referrer" content="never">\n<title></title>\n<link rel="stylesheet" href="style.css" />\n<script src="script.js"></script>\n<style>\n</style>\n<script>\nonReady = function() {\n};\nwindow.addEventListener(\'DOMContentLoaded\', onReady, true);\n</script>\n</head>\n<body>\n\n</body>\n</html>\n'
 ];
 DebugJS.FEATURES = [
   'togglableShowHide',
